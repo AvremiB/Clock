8e6d7e91a5daa9010d2ce2eff447acbb7ec207c5
AvremiB <avrahamanshin@gmail.com>
2024-11-13 20:45:22 +0200

    Update Clock.js

--- Clock.js
+++ Clock.js
@@ -16,34 +16,12 @@ function getFixedTime(startingFrom, floor) {
 	};
 }
 
-export default class Clock {
+class Clock {
 	constructor(startPoint) {
 		let radius = maxY * 0.4;
 		if (radius * 2.2 > maxX) radius = maxX / 2.2;
 		this.radius = radius;
 		this.center = new Point(maxX / 2, maxY / 2);
-
-		this.sketchBorder();
-		this.sketchDigits();
-		this.sketchGraduations();
-		this.sketchPivot();
-
-		this.createPointers(radius);
-
-		this.isRunning = false;
-
-		if (startPoint) {
-			this.start = startPoint;
-		} else {
-			const now =
-				new Date().getTime() / 1000 - new Date().getTimezoneOffset() * 60;
-			this.start = now % (12 * 60 * 60);
-		}
-		this.originStart = this.start;
-		this.animationFrameId = null;
-	}
-
-	createPointers(radius) {
 		this.hPointer = new ClockPointer('hours', 60 * 60, 12, radius * 0.45, radius / 45);
 		this.mPointer = new ClockPointer(
 			'minutes',
@@ -61,6 +39,15 @@ export default class Clock {
 			radius / 160,
 			true
 		);
+		this.isRunning = false;
+		if (startPoint) this.start = startPoint;
+		else {
+			const now =
+				new Date().getTime() / 1000 - new Date().getTimezoneOffset() * 60;
+			this.start = now % (12 * 60 * 60);
+		}
+		this.originStart = this.start;
+		this.animationFrameId = null;
 	}
 
 	get pointers() {
@@ -68,18 +55,19 @@ export default class Clock {
 	}
 
 	getPointCoordinates(angle, distanceFromCenter) {
+		angle = toClockAngle(angle);
 		const x = this.center.x + Math.cos(angle) * distanceFromCenter;
 		const y = this.center.y + Math.sin(angle) * distanceFromCenter;
 		return [x, y];
 	}
 
-	drawAll() {
-		this.cleanUp();
+	draw() {
+		this.drawBase();
 		this.pointers.forEach((p) => p.draw());
 		this.drawPivot();
 	}
 
-	cleanUp() {
+	drawBase() {
 		ctx.fillStyle = colors.outerBackground;
 		ctx.fillRect(0, 0, maxX, maxY);
 		this.drawBorder();
@@ -87,32 +75,14 @@ export default class Clock {
 		this.drawGraduations();
 	}
 
-	sketchBorder() {
+	drawBorder() {
 		this.borderPath = new Path2D();
 		this.borderPath.arc(...this.center, this.radius, 0, WHOLE_ANGLE);
-	}
-
-	drawBorder() {
-		// border
 		ctx.lineWidth = this.radius / 18;
 		ctx.strokeStyle = colors.border;
 		ctx.stroke(this.borderPath);
-		// inner color
 		ctx.fillStyle = colors.innerBackground;
 		ctx.fill(this.borderPath);
-	}
-
-	sketchDigits() {
-		const distanceFromCenter = this.radius * 0.73;
-		this.digitsPaths = [];
-
-		for (let i = 1; i <= 12; i++) {
-			const angle = timeToRad(i, 12) - PI_HALF;
-			this.digitsPaths.push({
-				digit: i,
-				point: this.getPointCoordinates(angle, distanceFromCenter),
-			});
-		}
 	}
 
 	drawDigits() {
@@ -121,58 +91,46 @@ export default class Clock {
 		ctx.font = `${this.radius * 0.24}px narkisim`;
 		ctx.textAlign = 'center';
 		ctx.textBaseline = 'middle';
-
-		for (const { digit, point } in this.digitsPoints) {
-			ctx.strokeText(digit.toString(), ...point);
-		}
-	}
-
-	sketchGraduations() {
-		this.graduations = [];
-
-		for (let i = 0; i < 60; i++) {
-			const grad = { path: new Path2D() };
-			this.graduations.push(grad);
-			grad.angle = timeToRad(i, 60);
-
-			let startDistance;
-			if (i % 5 === 0) {
-				startDistance = this.radius * 0.86;
-				grad.length = this.radius * 0.08;
-				// width and color
-				grad.lineWidth = this.radius / 150;
-				grad.strokeStyle = colors.hourGraduation;
-			} else {
-				startDistance = this.radius * 0.89;
-				grad.length = this.radius * 0.05;
-				// width and color
-				ctx.lineWidth = this.radius / 200;
-				ctx.strokeStyle = colors.graduation;
-			}
-
-			const start = this.getPointCoordinates(angle, startDistance);
-			const end = this.getPointCoordinates(angle, startDistance + grad.length);
-			grad.path.moveTo(start);
-			grad.path.lineTo(end);
+		const distanceFromCenter = this.radius * 0.73;
+		for (let i = 1; i <= 12; i++) {
+			const angle = timeToRad(i, 12);
+			ctx.strokeText(
+				i.toString(),
+				...this.getPointCoordinates(angle, distanceFromCenter)
+			);
 		}
 	}
 
 	drawGraduations() {
-		this.graduations.forEach((g) => {
-			ctx.lineWidth = g.lineWidth;
-			ctx.strokeStyle = g.strokeStyle;
-			ctx.stroke(g.path);
-		});
-	}
+		for (let i = 0; i < 60; i++) {
+			const angle = timeToRad(i, 60);
+			let distanceFromCenter, length;
 
-	sketchPivot() {
-		this.pivot = new Path2D();
-		this.pivot.arc(...this.center, this.radius / 50, 0, WHOLE_ANGLE);
+			if (i % 5 === 0) {
+				distanceFromCenter = this.radius * 0.86;
+				length = this.radius * 0.08;
+				ctx.lineWidth = this.radius / 150;
+				ctx.strokeStyle = colors.hourGraduation;
+			} else {
+				distanceFromCenter = this.radius * 0.89;
+				length = this.radius * 0.05;
+				ctx.lineWidth = this.radius / 200;
+				ctx.strokeStyle = colors.graduation;
+			}
+
+			ctx.beginPath();
+			ctx.moveTo(...this.getPointCoordinates(angle, distanceFromCenter));
+			ctx.lineTo(...this.getPointCoordinates(angle, distanceFromCenter + length));
+			ctx.stroke();
+		}
 	}
 
 	drawPivot() {
+		ctx.beginPath();
+		ctx.arc(...this.center, this.radius / 50, 0, WHOLE_ANGLE);
+		ctx.closePath();
 		ctx.fillStyle = colors.pivot;
-		ctx.fill(this.pivot);
+		ctx.fill();
 	}
 
 	run() {
@@ -182,19 +140,16 @@ export default class Clock {
 	}
 
 	update(singleTick = false, includeSeconds = true) {
-		const newTime = getFixedTime(
-			this.start,
-			preferences.rhythm === 'tick' && this.isRunning
-		);
+		const newTime = getFixedTime(preferences.rhythm === 'tick' && clock.isRunning);
 
 		this.pointers.forEach((pointer) => {
 			pointer.value = newTime[pointer.name];
-			if (pointer.name !== 'seconds' || includeSeconds) {
-				pointer.angle = timeToRad(pointer.value, pointer.max) - PI_HALF;
-			}
+			//	if (pointer.name !== 'seconds' || includeSeconds) {
+			pointer.angle = timeToRad(pointer.value, pointer.max);
+			//	}
 		});
 
-		this.drawAll();
+		this.draw();
 		if (!singleTick) {
 			this.animationFrameId = requestAnimationFrame(() => this.update());
 		}
